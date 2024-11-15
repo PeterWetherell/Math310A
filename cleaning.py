b0282c137d890f12ba7138990a2f5281d146c4da
dinomasater186 <peterwetherell186@gmail.com>
2024-11-14 21:38:53 -0700

    working on adding more noise to decrease similarity

--- cleaning.py
+++ cleaning.py
@@ -17,8 +17,8 @@ def process_file(input_file, output_file):
         file.write(stripped_content)
 
 # Specify input and output file names
-input_filename = 'Transcriptions/pnp_part1_transcription_noisy.txt'  # Change this to your input file name
-output_filename = 'Transcriptions/pnp_part1_cleaned_noisy.txt'  # Change this to your desired output file name
+input_filename = 'Transcriptions/pnp_part2_transcription.txt'  # Change this to your input file name
+output_filename = 'Transcriptions/pnp_part2_cleaned.txt'  # Change this to your desired output file name
 
 # Process the file
 process_file(input_filename, output_filename)
--- transcription.py
+++ transcription.py
@@ -9,7 +9,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def transcribe(audio_file):
     model=whisper.load_model("tiny")
     result = model.transcribe(audio_file)
-    with open('Transcriptions/pnp_part2_transcription_noisy.txt','w') as file:
+    with open('Transcriptions/pnp_part2_transcription_noisy.txt','w', encoding="utf-8") as file:
         file.write(result['text'])
     #print(result)
-transcribe("NormalizedSoundData/Noisy/PAP2.wav")
+transcribe("./NormalizedSoundData/Noisy/PAP2.wav")
