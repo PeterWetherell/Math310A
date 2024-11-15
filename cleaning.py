00f04c1a8854f170643a01afe14b9e605076b258
dinomasater186 <peterwetherell186@gmail.com>
2024-11-14 21:54:35 -0700

    added stop word filtering for cleaning

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,22 @@
 import string
 
+#https://gist.github.com/sebleier/554280
+stop_words = {
+    "i",        "me",       "my",           "myself",       "we",       "our",      "ours",     "ourselves",    "you",          "your",
+    "yours",    "yourself", "yourselves",   "he",           "him",      "his",      "himself",  "she",          "her",          "hers",
+    "herself",  "it",       "its",          "itself",       "they",     "them",     "their",    "theirs",       "themselves",   "what",
+    "which",    "who",      "whom",         "this",         "that",     "these",    "those",    "am",           "is",           "are",
+    "was",      "were",     "be",           "been",         "being",    "have",     "has",      "had",          "having",       "do",
+    "does",     "did",      "doing",        "a",            "an",       "the",      "and",      "but",          "if",           "or",
+    "because",  "as",       "until",        "while",        "of",       "at",       "by",       "for",          "with",         "about",
+    "against",  "between",  "into",         "through",      "during",   "before",   "after",    "above",        "below",        "to",
+    "from",     "up",       "down",         "in",           "out",      "on",       "off",      "over",         "under",        "again",
+    "further",  "then",     "once",         "here",         "there",    "when",     "where",    "why",          "how",          "all",
+    "any",      "both",     "each",         "few",          "more",     "most",     "other",    "some",         "such",         "no",
+    "nor",      "not",      "only",         "own",          "same",     "so",       "than",     "too",          "very",         "can",
+    "will",     "just",     "don't",        "should",       "now"
+}
+
 def process_file(input_file, output_file):
 
     # Open the input file for reading
@@ -12,13 +29,20 @@ def process_file(input_file, output_file):
     translator = str.maketrans('', '', string.punctuation)
     stripped_content = content.translate(translator).lower()
 
+
+    # Remove stop words
+    filtered_words = [
+        word for word in stripped_content.split() if word not in stop_words
+    ]
+    processed_content = ' '.join(filtered_words)
+
     # Write the processed content to the output file
-    with open(output_filename, 'w') as file:
-        file.write(stripped_content)
+    with open(output_filename, 'w', encoding='utf-8') as file:
+        file.write(processed_content)
 
 # Specify input and output file names
-input_filename = 'Transcriptions/pnp_part2_transcription.txt'  # Change this to your input file name
-output_filename = 'Transcriptions/pnp_part2_cleaned.txt'  # Change this to your desired output file name
+input_filename = 'Transcriptions/pnp_part2_transcription_noisy.txt'  # Change this to your input file name
+output_filename = 'Transcriptions/pnp_part2_cleaned_noisy.txt'  # Change this to your desired output file name
 
 # Process the file
 process_file(input_filename, output_filename)
