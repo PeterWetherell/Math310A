bb82de5f4237ea6c759ccd99123beda864b02313
dinomasater186 <peterwetherell186@gmail.com>
2024-11-18 14:54:20 -0700

    resolved merge

--- model.py
+++ model.py
@@ -50,29 +50,39 @@ segment_length = 10 # Number of seconds in the segment length
 segment_frames = segment_length * fr1
 num_segments = len(wav_x_data) // segment_frames
 
+sub_segment_order = np.linspace(0,num_segments * 10 - 1,num_segments * 10) # Get a list of each second we can choose from
+np.random.shuffle(sub_segment_order) # Shuffle it arround
+
+#print(sub_segment_order)
+
 for seg_num in range(num_segments):
-    # Convert a specific segment into spectrograms (we don't have the memory to convert all 6 hours)
-    print(f'Converting segment {seg_num} of {num_segments} from raw data into STFT')
-    x_data_complex = ProjectUtils.compute_STFT(wav_x_data[seg_num*segment_frames:(seg_num+1)*segment_frames], width)
-    y_data_complex = ProjectUtils.compute_STFT(wav_y_data[seg_num*segment_frames:(seg_num+1)*segment_frames], width)
-
-    if (x_data_complex.shape != y_data_complex.shape):
-        print("Error with conversion into STFT. Data must have the same shape")
-        exit()
-
-    # Convert the spectrograms into amplitude only (abs does magnitude for some reason)
-    print("Converting STFT to amplitude only")
-    x_data = np.abs(x_data_complex)
-    y_data = np.abs(y_data_complex)
-
-    # Convert into windows with corresponding targets
-    print("Switching into windowed data for training")
-    data_length = x_data.shape[0]
     input_windows = []
     targets = []
-    for i in range(data_length - height + 1):
-        input_windows.append(x_data[i:i+height, :])
-        targets.append(y_data[int(i+height/2), :])
+    
+    print(f'Converting segment {seg_num} from raw data into STFT')
+
+    for i in range(segment_length):
+        start = int(sub_segment_order[seg_num*10+i])
+        #print(start)
+        # Convert a specific segment into spectrograms (we don't have the memory to convert all 6 hours)
+        x_data_complex = ProjectUtils.compute_STFT(wav_x_data[start*fr1:(start + 1)*fr1], width)
+        y_data_complex = ProjectUtils.compute_STFT(wav_y_data[start*fr1:(start + 1)*fr1], width)
+
+        if (x_data_complex.shape != y_data_complex.shape):
+            print("Error with conversion into STFT. Data must have the same shape")
+            exit()
+
+        # Convert the spectrograms into amplitude only (abs does magnitude for some reason)
+        #print("Converting STFT to amplitude only")
+        x_data = np.abs(x_data_complex)
+        y_data = np.abs(y_data_complex)
+
+        # Convert into windows with corresponding targets
+        #print("Switching into windowed data for training")
+        data_length = x_data.shape[0]
+        for i in range(data_length - height + 1):
+            input_windows.append(x_data[i:i+height, :])
+            targets.append(y_data[int(i+height/2), :])
 
     # Convert from python list to numpy list
     input_windows = np.array(input_windows)
