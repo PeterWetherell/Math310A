93d6a88225e67199e3c78cd827820261baf4c3c2
Christine Worley <christineworley@Christines-MacBook-Pro-3.local>
2024-11-22 14:18:08 -0700

    added model 5

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 # model.add(Dropout(0.25))
 
 # Flatten and add dense layers
-model.add(BatchNormalization())
+#model.add(BatchNormalization())
 model.add(Flatten())
 model.add(Dense(units=256, activation='relu'))
 model.add(Dropout(0.2))
@@ -106,5 +106,5 @@ for seg_num in range(num_segments):
     # Train the model
     model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val))
     if (seg_num % num_segments_per_save == 0 and seg_num != 0):
-        model.save(f'./Models/deonoiserV5-{seg_num // num_segments_per_save}-0.keras')
-model.save(f'./Models/deonoiserV5-{seg_num // num_segments_per_save}-{seg_num % num_segments_per_save}.keras')
+        model.save(f'./Models/deonoiserV6-{seg_num // num_segments_per_save}-0.keras')
+model.save(f'./Models/deonoiserV6-{seg_num // num_segments_per_save}-{seg_num % num_segments_per_save}.keras')
