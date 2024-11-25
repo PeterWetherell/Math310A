5a956552b84f3b306c8c4b14ebb0cb80d4e6ca9f
Christine Worley <christineworley@Christines-MacBook-Pro-3.local>
2024-11-25 16:05:47 -0700

    fixed it I think

--- transcription.py
+++ transcription.py
@@ -35,11 +35,12 @@ folder_path = "./Outputs"
 # Iterate over files in the folder
 
 folder_path = "./OutputTranscriptions"
+output_folder = "./CleanedOutputTranscriptions"
 my_dict = {}
 for filename in os.listdir(folder_path):
     # Construct full file path
     text_path = os.path.join(folder_path, filename)
-    cleaned_path = os.path.splitext(text_path)[0]+"_cleaned"+ ".txt"
+    cleaned_path = os.path.join(output_folder,filename)
     cleaning.process_file(text_path, cleaned_path)
     model_sim = check.cosine_similarity_files("./Transcriptions/yellow_wallpaper_transcription.txt",cleaned_path)
     regular_sim = check.cosine_similarity_files("./Transcriptions/yellow_wallpaper_transcription.txt", "./Transcriptions/yellow_wallpaper_cleaned_noisy.txt")
