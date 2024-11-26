294c89c101cdb79b7027d1e7b449cbb42a75673d
Christine Worley <christineworley@Christines-MacBook-Pro-3.local>
2024-11-25 19:34:46 -0700

    tested whisper

--- donothing.py
+++ donothing.py
@@ -2,8 +2,12 @@ import ProjectUtils
 from scipy.io.wavfile import write
 import numpy as np
 
-wav_x_data,fr1 = ProjectUtils.load_wav("./NormalizedSoundData/Noisy/YWP.wav")
-x_data_complex = ProjectUtils.scipy_STFT(wav_x_data, fr1, 245)
-x_data = ProjectUtils.scipy_iSTFT(x_data_complex, fr1, 245)
-reconstructed_audio_int16 = (x_data * 32767).astype(np.int16)
+data,fr1 = ProjectUtils.load_wav("./NormalizedSoundData/Noisy/YWP.wav")
+from scipy.io import wavfile
+
+# Read a WAV file
+# fr1, data = wavfile.read("./NormalizedSoundData/Noisy/YWP.wav")
+# x_data_complex = ProjectUtils.scipy_STFT(data, fr1, 245)
+# x_data = ProjectUtils.scipy_iSTFT(x_data_complex, fr1, 245)
+reconstructed_audio_int16 = (data * 32767).astype(np.int16)
 write("./Outputs/donothing.wav", 44100, reconstructed_audio_int16)
--- transcription.py
+++ transcription.py
@@ -1,9 +1,9 @@
 import ssl
 import whisper
 import time
-# import cleaning
-# import check
-# import os
+import cleaning
+import check
+import os
 from urllib import request
 
 # Disable SSL verification globally
@@ -21,32 +21,34 @@ def transcribe(audio_file, textfile):
     with open(textfile,'w', encoding="utf-8") as file:
         file.write(result['text'])
     #print(result)
-transcribe("./NormalizedSoundData/Clean/YWP.wav", "./Transcriptions/YWP.txt")
+# transcribe("./Outputs/donothing.wav", "./OutputTranscriptions/donothing.txt")
 # Specify the folder path
 
-
-# for filename in os.listdir(folder_path):
-#     # Construct full file path
-#     file_path = os.path.join(folder_path, filename)
-#     file_root, file_extension = os.path.splitext(filename)
-#     text_path = os.path.join("./OutputTranscriptions", file_root) + '.txt'
-#     print("transcribing", filename)
-#     transcribe(file_path, text_path)
+folder_path = "./Outputs"
+for filename in os.listdir(folder_path):
+    # Construct full file path
+    file_path = os.path.join(folder_path, filename)
+    file_root, file_extension = os.path.splitext(filename)
+    text_path1 = os.path.join("./OutputTranscriptions", file_root) + '1.txt'
+    text_path2 = os.path.join("./OutputTranscriptions", file_root) + '2.txt'
+    print("transcribing", filename)
+    transcribe(file_path, text_path1)
+    transcribe(file_path, text_path2)
 # Iterate over files in the folder
 
-# folder_path = "./OutputTranscriptions"
-# output_folder = "./CleanedOutputTranscriptions"
-# my_dict = {}
-# for filename in os.listdir(folder_path):
-#     # Construct full file path
-#     text_path = os.path.join(folder_path, filename)
-#     cleaned_path = os.path.join(output_folder,filename)
-#     cleaning.process_file(text_path, cleaned_path)
-#     model_sim = check.cosine_similarity_files("./Transcriptions/yellow_wallpaper_transcription.txt",cleaned_path)
-#     regular_sim = check.cosine_similarity_files("./Transcriptions/yellow_wallpaper_transcription.txt", "./Transcriptions/yellow_wallpaper_cleaned_noisy.txt")
-#     my_dict[filename] = model_sim-regular_sim
+folder_path = "./OutputTranscriptions"
+output_folder = "./CleanedOutputTranscriptions"
+my_dict = {}
+for filename in os.listdir(folder_path):
+    # Construct full file path
+    text_path = os.path.join(folder_path, filename)
+    cleaned_path = os.path.join(output_folder,filename)
+    cleaning.process_file(text_path, cleaned_path)
+    model_sim = check.cosine_similarity_files("./Transcriptions/YWP_cleaned.txt",cleaned_path)
+    regular_sim = check.cosine_similarity_files("./Transcriptions/YWP_cleaned.txt", "./Transcriptions/yellow_wallpaper_cleaned_noisy.txt")
+    my_dict[filename] = model_sim-regular_sim
 
-# # Sort dictionary by values (ascending)
-# sorted_dict = {k: v for k, v in sorted(my_dict.items(), key=lambda item: item[1])}
-# for i in sorted_dict:
-#     print(i, " ", sorted_dict[i])
+# Sort dictionary by values (ascending)
+sorted_dict = {k: v for k, v in sorted(my_dict.items(), key=lambda item: item[1])}
+for i in sorted_dict:
+    print(i, " ", sorted_dict[i])
